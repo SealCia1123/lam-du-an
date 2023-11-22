59029d01349b4d55a0cb97c9a10d0ef1fc60d0a7
SealCia1123 <138353079+SealCia1123@users.noreply.github.com>
2023-11-22 11:23:58 +0700

    update project

--- project.py
+++ project.py
@@ -4,16 +4,13 @@ import matplotlib.pyplot as plt
 from functools import reduce
 
 # Phần thêm data từ file excel và merge data
-df1 = pd.read_excel("D:/lam-du-an/FPT_23.10_17.11.xlsx")
-df2 = pd.read_excel("D:/lam-du-an/FPT_25.09_20.10.xlsx")
-df3 = pd.read_excel("D:/lam-du-an/FPT_24.08_24.09.xlsx")
-df4 = pd.read_excel("D:/lam-du-an/FPT_27.07_23.08.xlsx")
-df5 = pd.read_excel("D:/lam-du-an/FPT_29.06_26.07.xlsx")
-df6 = pd.read_excel("D:/lam-du-an/FPT_01.06_28.06.xlsx")
-df7 = pd.read_excel("D:/lam-du-an/FPT_17.05_31.05.xlsx")
+df1 = pd.read_excel("D:/lam-du-an/Phan1.xlsx")
+df2 = pd.read_excel("D:/lam-du-an/Phan2.xlsx")
+df3 = pd.read_excel("D:/lam-du-an/Phan3.xlsx")
+df4 = pd.read_excel("D:/lam-du-an/Phan4.xlsx")
 
 # Merge data của 6 tháng lại thành 1 dataframe tổng
-dfl = [df1, df2, df3, df4, df5, df6, df7]
+dfl = [df1, df2, df3, df4]
 df_merged = reduce(lambda left, right: pd.merge(left, right, how="outer"), dfl)
 
 # Sửa lỗi 00:00 trong file excel
@@ -21,10 +18,12 @@ df_merged['Ngày'] = pd.to_datetime(df_merged['Ngày'])
 df_merged['Ngày'] = df_merged['Ngày'].astype(str)
 
 # Tính giá cổ phiếu theo ngày
-df_merged['Giá cổ phiếu theo ngày (VNĐ)'] = (df_merged['Bán: Giá trị (tỷ VNĐ)'] * 1000000000) / df_merged['Bán: Khối lượng']
+df_merged['Giá cổ phiếu theo ngày (VNĐ)'] = (df_merged['Bán: Giá trị (tỷ VNĐ)'] * 1000000000) / df_merged[
+    'Bán: Khối lượng']
 df_merged = df_merged.fillna(0)
 
-# Tính các giá trị trung bình, phương sai, độ lệch chuẩn, tứ phân vị của các cột: kl giá trị ròng, kl mua, kl bán (câu 4)
+# Tính các giá trị trung bình, phương sai, độ lệch chuẩn, tứ phân vị của các cột:
+# kl giá trị ròng, kl mua, kl bán (câu 4)
 trung_binh = df_merged[['Giao dịch ròng: Khối lượng', 'Mua: Khối lượng', 'Bán: Khối lượng']].mean()
 phuong_sai = df_merged[['Giao dịch ròng: Khối lượng', 'Mua: Khối lượng', 'Bán: Khối lượng']].var()
 do_lech_chuan = df_merged[['Giao dịch ròng: Khối lượng', 'Mua: Khối lượng', 'Bán: Khối lượng']].std()
@@ -58,11 +57,11 @@ df_thamso['Khối lượng giao dịch bán'] = tham_so_khoi_luong_ban
 
 # Vẽ biểu đồ
 print(df_merged)
-#plt.plot(df_merged['Mua: Khối lượng'])
-# plt.xlabel('Ngày')
-# plt.ylabel('Khối lượng')
-# plt.title('Xu hướng mua của khối ngoại')
-# plt.show()
+plt.plot(df_merged['Mua: Khối lượng'])
+plt.xlabel('Ngày')
+plt.ylabel('Khối lượng')
+plt.title('Xu hướng mua của khối ngoại')
+plt.show()
 
 # Xuất ra file excel và căn chỉnh khoảng cách của column
 # Phải cài thư viện xlsxwriter
